9a33be0160fd4aa82880fe57ae5d967426b18367
pmatyus <>
2021-05-05 15:13:22 +0000

    Exercise solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    age = int(input("How old are you?"))
+    if (age >= 18):
+      print("You are an adult")
+    else:
+      print("You are not an adult")
 
 if __name__ == '__main__':
     main()
